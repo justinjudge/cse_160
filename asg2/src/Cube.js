d3a5ea5d11c9871b616dcdc3d5002769db647b68
justinjudge <justinsjudge@gmail.com>
2025-02-04 16:08:02 -0800

    asg2 mostly complete without creature

---
+++ asg2/src/Cube.js
@@ -0,0 +1,71 @@
+class Cube {
+    constructor() {
+        this.type = 'cube';
+        // this.position = [0.0, 0.0, 0.0];
+        this.color = [1.0, 1.0, 1.0, 1.0];
+        // this.size = 5;
+        // this.segments = 10;
+        this.matrix = new Matrix4();
+    }
+
+    render() {
+        // var xy = this.position;
+        var rgba = this.color;
+        // var size = this.size;
+        // var segments = this.segments;
+
+        // Pass the color of a point to u_FragColor variable
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+        // Pass the matrix to a u_ModelMatrix attribute
+        gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+        // Draw
+        /*
+        var d = this.size/200.0; // delta
+
+        let angleStep = 360/this.segments;
+        for (var angle = 0; angle < 360; angle = angle + angleStep) {
+            let centerPt = [xy[0], xy[1]];
+            let angle1 = angle;
+            let angle2 = angle + angleStep;
+            let vec1 = [Math.cos(angle1 * Math.PI/180)*d, Math.sin(angle1 * Math.PI/180)*d];
+            let vec2 = [Math.cos(angle2 * Math.PI/180)*d, Math.sin(angle2 * Math.PI/180)*d];
+            let pt1 = [centerPt[0]+vec1[0], centerPt[1]+vec1[1]];
+            let pt2 = [centerPt[0]+vec2[0], centerPt[1]+vec2[1]];
+
+            drawTriangle( [xy[0], xy[1], pt1[0], pt1[1], pt2[0], pt2[1]] );
+        }
+        */
+
+        // Front of cube
+        drawTriangle3D( [0, 0, 0,     1, 1, 0,      1, 0, 0 ] );
+        drawTriangle3D( [0, 0, 0,     0, 1, 0,      1, 1, 0 ] );
+
+        // Pass the color of a point to a u_FragColor unfirom variable
+        gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
+
+        // Top of cube
+        drawTriangle3D( [0, 1, 0,     0, 1, 1,      1, 1, 1 ] );
+        drawTriangle3D( [0, 1, 0,     1, 1, 1,      1, 1, 0 ] );
+
+        // Bottom of cube
+        drawTriangle3D( [0, 0, 0,     0, 0, 1,      1, 0, 1 ] );
+        drawTriangle3D( [0, 0, 0,     1, 0, 1,      1, 0, 0 ] );
+
+        // Back of cube
+        drawTriangle3D( [0, 0, 1,     1, 1, 1,      1, 0, 1 ] );
+        drawTriangle3D( [0, 0, 1,     0, 1, 1,      1, 1, 1 ] );
+
+        // Pass the color of a point to a u_FragColor unfirom variable
+        gl.uniform4f(u_FragColor, rgba[0]*.8, rgba[1]*.8, rgba[2]*.8, rgba[3]);
+
+        // Left of cube
+        drawTriangle3D( [0, 0, 0,     0, 1, 0,      0, 1, 1 ] );
+        drawTriangle3D( [0, 0, 0,     0, 1, 1,      0, 0, 1 ] );
+
+        // Right of cube
+        drawTriangle3D( [1, 0, 0,     1, 1, 0,      1, 1, 1 ] );
+        drawTriangle3D( [1, 0, 0,     1, 1, 1,      1, 0, 1 ] );
+    }
+}
--- asg2/src/asg2.js
+++ asg2/src/asg2.js
@@ -2,10 +2,10 @@
 // Vertex shader program
 var VSHADER_SOURCE = `
   attribute vec4 a_Position;
-  uniform float u_Size;
+  uniform mat4 u_ModelMatrix;
+  uniform mat4 u_GloablRotateMatrix;
   void main() {
-    gl_Position = a_Position;
-    gl_PointSize = u_Size;
+    gl_Position = u_GloablRotateMatrix * u_ModelMatrix * a_Position;
   }`
 
 // Fragment shader program
@@ -22,6 +22,8 @@ let gl;
 let a_Position;
 let u_FragColor;
 let u_Size;
+let u_ModelMatrix;
+let u_GloablRotateMatrix
 
 function setupWebGL() {
   // Retrieve <canvas> element
@@ -38,7 +40,10 @@ function setupWebGL() {
 
   // to get alpha transparency to work
   gl.enable(gl.BLEND); 
-  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA); 
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+  // to fix depth of shapes in 3d
+  gl.enable(gl.DEPTH_TEST);
 }
 
 function connectVariablesToGLSL() {
@@ -62,12 +67,23 @@ function connectVariablesToGLSL() {
     return;
   }
 
-  // Get the storage location of u_Size
-  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
-  if (!u_Size) {
-    console.log('Failed to get the storage location of u_Size');
+  // Get the storage location of u_ModelMatrix
+  u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+  if (!u_ModelMatrix) {
+    console.log('Failed to get the storage location of u_ModelMatrix');
     return;
   }
+
+  // Get the storage location of u_GloablRotateMatrix
+  u_GloablRotateMatrix = gl.getUniformLocation(gl.program, 'u_GloablRotateMatrix');
+  if (!u_GloablRotateMatrix) {
+    console.log('Failed to get the storage location of u_GloablRotateMatrix');
+    return;
+  }
+
+  // set an initial value for this matrix to identity
+  var identityM = new Matrix4();
+  gl.uniformMatrix4fv(u_ModelMatrix, false, identityM.elements);
 }
 
 // Constants
@@ -79,7 +95,13 @@ const CIRCLE = 2;
 let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
 let g_selectedSize = 5;
 let g_selectedType = POINT;
-let g_selectedSegments = 10;
+let g_globalAngle = 0;
+let g_yellowAngle = 0;
+let g_magentaAngle = 0;
+let g_yellowAnimation = false;
+let g_magentaAnimation = false;
+let g_isMouseDown = false;
+// let g_selectedSegments = 10;
 
 // Set up actions for the HTML UI elements
 function addActionsForHtmlUI() {
@@ -103,6 +125,23 @@ function addActionsForHtmlUI() {
   // Transparency
   document.getElementById("transSlide").addEventListener('mouseup', function() { g_selectedColor[3] = this.value/100; });
 
+  // Button events
+  document.getElementById('animationYellowOffButton').onclick = function() {g_yellowAnimation = false;};
+  document.getElementById('animationYellowOnButton').onclick = function() {g_yellowAnimation = true;};
+  document.getElementById('animationMagentaOffButton').onclick = function() {g_magentaAnimation = false;};
+  document.getElementById('animationMagentaOnButton').onclick = function() {g_magentaAnimation = true;};
+
+  // Angle Slider
+  document.getElementById("angleSlide").addEventListener('mousemove', function() { g_globalAngle = this.value; renderAllShapes(); });
+
+  // Mouse Control
+
+  // Yellow Slider
+  document.getElementById("yellowSlide").addEventListener('mousemove', function() { g_yellowAngle = this.value; renderAllShapes(); });
+
+  // Magenta Slider
+  document.getElementById("magentaSlide").addEventListener('mousemove', function() { g_magentaAngle = this.value; renderAllShapes(); });
+
   // Size Slider Events
   document.getElementById("sizeSlide").addEventListener('mouseup', function() { g_selectedSize = this.value; });
   document.getElementById("segmentSlide").addEventListener('mouseup', function() { g_selectedSegments = this.value; });
@@ -125,7 +164,29 @@ function main() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  // gl.clear(gl.COLOR_BUFFER_BIT);
+  // he commented out above and put 
+  // renderAllShapes();
+  requestAnimationFrame(tick);
+}
+
+var g_startTime = performance.now()/1000.0;
+var g_seconds = performance.now()/1000.0 - g_startTime;
+
+// Called by the browser repeatedly whenever its time
+function tick() {
+  // Save the current time
+  g_seconds = performance.now()/1000.0 - g_startTime;
+  console.log(g_seconds);
+
+  // Update Animation Angles
+  updateAnimationAngles();
+
+  // Draw everything
+  renderAllShapes();
+
+  // Tell the browser to update again when it ahs time
+  requestAnimationFrame(tick);
 }
 
 var g_shapesList = [];
@@ -232,17 +293,81 @@ function convertCoordinatesEventToGL(ev) {
   return([x, y]);
 }
 
+// Update the angles of everything if currently animated
+function updateAnimationAngles() {
+  if (g_yellowAnimation) {
+    g_yellowAngle = (45*Math.sin(g_seconds));
+  }
+  if (g_magentaAnimation) {
+    g_magentaAngle = (45*Math.sin(3*g_seconds));
+  }
+}
+
 // Draw every shape that is supposed to be in the canvas
 function renderAllShapes() {
+
+  // Pass the matrix to u_ModelMatrix attribute
+  var gloablRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+  gl.uniformMatrix4fv(u_GloablRotateMatrix, false, gloablRotMat.elements);
+
   // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   //var len = g_points.length;
-  var len = g_shapesList.length;
+  /*var len = g_shapesList.length;
 
   for(var i = 0; i < len; i++) {
 
     g_shapesList[i].render();
   
-  }
+  }*/
+
+  // Draw a test triangle
+  // drawTriangle3D( [-1.0, 0.0, 0.0,    -0.5, -1.0, 0.0,   0.0, 0.0, 0.0] );
+
+  // Draw the body cube
+  var body = new Cube();
+  body.color = [1.0, 0.0, 0.0, 1.0];
+  body.matrix.setTranslate(-0.25, -0.75, 0.0);
+  body.matrix.rotate(-5, 1, 0, 0);
+  body.matrix.scale(0.5, 0.3, 0.5);
+  body.render();
+
+  // Draw a left arm
+  var leftArm = new Cube();
+  leftArm.color = [1, 1, 0, 1];
+  leftArm.matrix.setTranslate(0, -0.5, 0.0);
+  leftArm.matrix.rotate(-5, 1, 0, 0);
+  leftArm.matrix.rotate(-g_yellowAngle, 0, 0, 1);
+  //if (g_yellowAnimation) {
+  //  leftArm.matrix.rotate(45*Math.sin(g_seconds), 0, 0, 1);
+  //} else {
+  //  leftArm.matrix.rotate(-g_yellowAngle, 0, 0, 1);
+  //}
+  var yellowCoordinatesMatrix = new Matrix4(leftArm.matrix);
+  leftArm.matrix.scale(0.25, 0.7, 0.5);
+  leftArm.matrix.translate(-0.5, 0, 0);
+  leftArm.render();
+
+  // Test box
+  var box = new Cube();
+  box.color = [1, 0, 1, 1];
+  box.matrix = yellowCoordinatesMatrix;
+  box.matrix.translate(0, 0.65, 0);
+  box.matrix.rotate(g_magentaAngle, 0, 0, 1);
+  var purpleCoordinatesMatrix = new Matrix4(box.matrix);
+  box.matrix.scale(0.3, 0.3, 0.3);
+  box.matrix.translate(-0.5, 0, -0.001);
+  box.render();
+
+  // Draw the tube
+  var tube = new Cylinder();
+  tube.color = [0.0, 1.0, 0.0, 1.0];
+  tube.matrix = purpleCoordinatesMatrix;
+  tube.matrix.scale(0.3, 0.3, 0.3);
+  tube.matrix.rotate(90, 1, 0, 0);
+  tube.matrix.translate(-0.5, 0.0, -2.0);
+  tube.render();
+  
 }
